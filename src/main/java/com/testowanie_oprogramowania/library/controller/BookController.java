d100964e56866f1209428b362f216ac7e5751dc1
karwowsm <karwowski.mat@gmail.com>
2018-12-08 17:58:33 +0100

    GET /books - BookController test passed

--- src/main/java/com/testowanie_oprogramowania/library/controller/BookController.java
+++ src/main/java/com/testowanie_oprogramowania/library/controller/BookController.java
@@ -1,9 +1,29 @@
 package com.testowanie_oprogramowania.library.controller;
 
+import com.testowanie_oprogramowania.library.entity.Book;
+import com.testowanie_oprogramowania.library.service.BookService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author Mateusz
  */
-class BookController {
-    
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public ResponseEntity<List<Book>> getAllBooks() {
+        return new ResponseEntity(bookService.getAllBooks(), HttpStatus.OK);
+    }
+
 }
