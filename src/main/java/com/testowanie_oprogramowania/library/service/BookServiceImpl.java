cc6dc93c7aff8b47c5a34c254ccaeb8882faad8f
karwowsm <karwowski.mat@gmail.com>
2018-12-08 17:54:36 +0100

    GET /books - BookServiceImpl test passed

--- src/main/java/com/testowanie_oprogramowania/library/service/BookServiceImpl.java
+++ src/main/java/com/testowanie_oprogramowania/library/service/BookServiceImpl.java
@@ -1,14 +1,20 @@
 package com.testowanie_oprogramowania.library.service;
 
 import com.testowanie_oprogramowania.library.entity.Book;
+import com.testowanie_oprogramowania.library.repository.BookRepository;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-
+@Service
 public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
 
     @Override
     public List<Book> getAllBooks() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return (List<Book>) bookRepository.findAll();
     }
-    
+
 }
