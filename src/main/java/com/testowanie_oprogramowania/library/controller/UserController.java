7f8e25a4ee5cc206c8ecac078b9727f7a8fedd07
karwowsm <karwowski.mat@gmail.com>
2018-12-09 10:38:33 +0100

    GET /users/{id}/borrowings - tests passed

--- src/main/java/com/testowanie_oprogramowania/library/controller/UserController.java
+++ src/main/java/com/testowanie_oprogramowania/library/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.testowanie_oprogramowania.library.controller;
 
+import com.testowanie_oprogramowania.library.entity.User;
 import com.testowanie_oprogramowania.library.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +17,12 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @GetMapping("/{id}/borrowings")
+    public ResponseEntity getBorrowings(@PathVariable("id") Long id) {
+        User user = userService.getUser(id);
+        if (user != null) {
+            return new ResponseEntity(userService.getBorrowings(id), HttpStatus.OK);
+        }
+        return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
 }
--- src/main/java/com/testowanie_oprogramowania/library/service/UserServiceImpl.java
+++ src/main/java/com/testowanie_oprogramowania/library/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.testowanie_oprogramowania.library.service;
 
 import com.testowanie_oprogramowania.library.entity.BookBorrowing;
 import com.testowanie_oprogramowania.library.entity.User;
+import com.testowanie_oprogramowania.library.repository.BookBorrowingRepository;
 import com.testowanie_oprogramowania.library.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,14 +15,17 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BookBorrowingRepository bookBorrowingRepository;
+
     @Override
     public User getUser(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return userRepository.findById(id).orElse(null);
     }
 
     @Override
     public List<BookBorrowing> getBorrowings(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return bookBorrowingRepository.findByUserId(id);
     }
 
 }
