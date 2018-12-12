66f39cf1c826217774ff814c8c6dcd1a860c8f1a
Jakub Barański <01123039@pw.edu.pl>
2018-12-12 01:23:30 +0100

    Commit unstaged changes

--- src/main/java/com/testowanie_oprogramowania/library/service/BookcopyService.java
+++
@@ -1,9 +0,0 @@
-package com.testowanie_oprogramowania.library.service;
-
-import com.testowanie_oprogramowania.library.entity.BookCopy;
-
-public interface BookCopyService {
-
-    BookCopy getBookcopyById(Long id);
-
-}
